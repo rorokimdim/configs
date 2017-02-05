e92db9229cfd6d1ad60773f4a54bd846f33f46e6
Amit Shrestha <ashrestha@housecanary.com>
2017-02-04 23:31:54 -0800

    Misc changes for bash and 4clojure.

--- emacs/customizations/ui.el
+++ emacs/customizations/ui.el
@@ -1,3 +1,4 @@
+
 ;; These customizations change the way emacs looks and disable/enable
 ;; some user interface elements. Some useful customizations are
 ;; commented out, and begin with the line "CUSTOMIZE". These are more
@@ -34,7 +35,7 @@
 ;; Uncomment the lines below by removing semicolons and play with the
 ;; values in order to set the width (in characters wide) and height
 ;; (in lines high) Emacs will have whenever you start it
-;; (setq initial-frame-alist '((top . 0) (left . 0) (width . 177) (height . 53)))
+(setq initial-frame-alist '((top . 0) (left . 0) (width . 177) (height . 53)))
 
 ;; These settings relate to how emacs interacts with your operating system
 (setq ;; makes killing/yanking interact with the clipboard
--- emacs/init.el
+++ emacs/init.el
@@ -10,6 +10,8 @@
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
 
 ;; Load and activate emacs packages. Do this first so that the
 ;; packages are loaded before you start trying to modify them.
@@ -137,3 +139,18 @@
 
 ;; Keyboard shortcuts
 (load "keys.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(package-selected-packages
+   (quote
+    (org-plus-contrib ## 4clojure tagedit smex smartscan rainbow-delimiters python-environment projectile paredit magit ido-ubiquitous flycheck-apertium exec-path-from-shell evil epc elpy clojure-mode-extra-font-locking cider-decompile auto-complete anaconda-mode alchemist))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
