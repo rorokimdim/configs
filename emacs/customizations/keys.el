a916b26208a33786ab751b499034df2761a7ce90
Amit Shrestha <ashrestha@housecanary.com>
2017-02-09 05:52:45 +0000

    Misc emacs configs.

--- emacs/customizations/keys.el
+++ emacs/customizations/keys.el
@@ -1,3 +1,10 @@
 ;; M-n and M-p for next/previous matches under cursor
 ;; M- to replace all matches under cursor
 (global-smartscan-mode t)
+
+(global-set-key [f3]
+                (lambda ()
+                  (interactive)
+                  (list-buffers)
+                  (delete-other-windows)
+                  (switch-to-buffer "*Buffer List*")))
