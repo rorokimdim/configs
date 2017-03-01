75a834429084937234d37ad68ee18c892e21c996
Amit Shrestha <ashrestha@housecanary.com>
2017-03-01 18:18:22 +0000

    Emacs: Fix clipboard for terminal sessions.

--- emacs/customizations/ui.el
+++ emacs/customizations/ui.el
@@ -20,21 +20,43 @@
 
 ;; These settings relate to how emacs interacts with your operating system
 (setq ;; makes killing/yanking interact with the clipboard
-      select-enable-clipboard t
-      select-enable-primary t
+ select-enable-clipboard t
+ select-enable-primary t
 
-      ;; Save clipboard strings into kill ring before replacing them.
-      ;; When one selects something in another program to paste it into Emacs,
-      ;; but kills something in Emacs before actually pasting it,
-      ;; this selection is gone unless this variable is non-nil
-      save-interprogram-paste-before-kill t
+ ;; Save clipboard strings into kill ring before replacing them.
+ ;; When one selects something in another program to paste it into Emacs,
+ ;; but kills something in Emacs before actually pasting it,
+ ;; this selection is gone unless this variable is non-nil
+ save-interprogram-paste-before-kill t
 
-      ;; Shows all options when running apropos. For more info,
-      ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Apropos.html
-      apropos-do-all t
+ ;; Shows all options when running apropos. For more info,
+ ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Apropos.html
+ apropos-do-all t
 
-      ;; Mouse yank commands yank at point instead of at click.
-      mouse-yank-at-point t)
+ ;; Mouse yank commands yank at point instead of at click.
+ mouse-yank-at-point t)
+
+;;
+;; Makes clipboard work in terminal sessions.
+;;
+(setq my-copy-command (cond ((eq system-type 'darwin) "pbcopy")
+                            ((or (eq system-type 'gnu/linux) (eq system-type 'linux)) "xsel -ib")))
+
+(setq my-paste-command (cond ((eq system-type 'darwin) "pbpaste")
+                             ((or (eq system-type 'gnu/linux) (eq system-type 'linux)) "xsel -ob")))
+
+(unless window-system
+  (when (getenv "DISPLAY")
+    (defun cut-function (text &optional push)
+      (with-temp-buffer
+        (insert text)
+        (call-process-region (point-min) (point-max) my-copy-command)))
+    (defun paste-function()
+      (let ((output (shell-command-to-string my-paste-command)))
+        (unless (string= (car kill-ring) output)
+          output)))
+    (setq interprogram-cut-function 'cut-function)
+    (setq interprogram-paste-function 'paste-function)))
 
 ;; No cursor blinking, it's distracting
 (blink-cursor-mode 0)
