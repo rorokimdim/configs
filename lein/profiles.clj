63484ed160c87e2d714d7f9183a8e4486ed3019f
Amit Shrestha <ashrestha@housecanary.com>
2017-03-19 00:30:41 -0700

    Lein: Cleanup dependencies in profiles.clj.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,14 +1,13 @@
 {
   :user {
     :plugins [
-      [lein-gorilla "0.3.6"]
+      [lein-gorilla "0.4.0"]
       [cider/cider-nrepl "0.15.0-SNAPSHOT"]
-      [luminus/lein-template "2.9.10.74"]
     ]
     :dependencies [
       [lein-cljfmt "0.5.6"]
       [org.clojure/tools.trace "0.7.9"]
-      [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+      [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
     ]
   }
 }
