313c345b57083184493e7c68ef343cd2e9c7a936
Amit Shrestha <ashrestha@housecanary.com>
2017-03-26 23:37:59 -0700

    Emacs: Follow symlinks to version controlled files.

--- emacs/customizations/misc.el
+++ emacs/customizations/misc.el
@@ -15,6 +15,9 @@
 (setq-default message-log-max nil)
 (kill-buffer "*Messages*")
 
+;; Follow symlinks to version controlled file without prompting
+(setq vc-follow-symlinks t)
+
 ;; List of times to show in helm-world-time
 (setq display-time-world-list '(("PST8PDT" "San Francisco")
                                 ("America/Chicago" "Chicago")
