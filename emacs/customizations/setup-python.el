b3081354e49e4e4b1ea6dc0eaa09f95ee9d81a37
Amit Shrestha <ashrestha@housecanary.com>
2017-04-11 17:10:52 -0700

    Emacs: More shorcuts for python shell.

--- emacs/customizations/setup-python.el
+++ emacs/customizations/setup-python.el
@@ -8,13 +8,26 @@
   (setq py-force-py-shell-name-p t)
   (modify-syntax-entry ?_ "w" python-mode-syntax-table)
   (evil-leader/set-key-for-mode 'python-mode
-    "r" (lambda ()
+    "r" 'run-python
+    "R" (lambda ()
           (interactive)
-          (run-python)
-          (switch-to-buffer "*Python*"))
+          (call-interactively 'run-python)
+          (call-interactively 'python-shell-switch-to-shell))
+    "eb" 'elpy-shell-send-region-or-buffer
+    "er" 'elpy-shell-send-region-or-buffer
+    "ec" (lambda ()
+           (interactive)
+           (let ((old-buffer (buffer-name)))
+             (switch-to-buffer "*Python*")
+             (comint-clear-buffer)
+             (switch-to-buffer old-buffer)))
+    "ef" 'python-shell-send-defun
     "gd" 'elpy-goto-definition
     "pd" 'elpy-doc
     "pt" 'my-python-add-breakpoint)
+  (add-hook 'inferior-python-mode-hook
+            (lambda ()
+              (local-set-key "\C-l" 'comint-clear-buffer)))
   (add-hook 'python-mode-hook
             (lambda ()
               (rainbow-delimiters-mode)
