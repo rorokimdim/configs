5132e5132401e4f2a77d9e3e53bd3ecfaad62068
Amit Shrestha <ashrestha@housecanary.com>
2017-04-16 01:30:41 -0700

    Emacs: Fix source code coloring in html exports.

--- emacs/customizations/setup-org.el
+++ emacs/customizations/setup-org.el
@@ -27,3 +27,20 @@
 
 ;; For exporting to reveal-js presentations
 (require 'ox-reveal)
+
+;; Fix source code coloring in html exports
+(defun my-org-inline-css-hook (exporter)
+  "Insert custom inline css to automatically set the
+ background of code to whatever theme I'm using's background"
+  (when (eq exporter 'html)
+    (let* ((my-pre-bg (face-background 'default))
+           (my-pre-fg (face-foreground 'default)))
+      (setq
+       org-html-head-extra
+       (concat
+        org-html-head-extra
+        (format
+         "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}
+          pre.src:before {background-color: %s; color: %s; top: 10px;}</style>\n"
+         my-pre-bg my-pre-fg my-pre-bg my-pre-fg))))))
+(add-hook 'org-export-before-processing-hook #'my-org-inline-css-hook)
