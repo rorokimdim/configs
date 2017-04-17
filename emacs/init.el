3bd7c98005d4e0b3b0db47da7806eae22c812765
Amit Shrestha <ashrestha@housecanary.com>
2017-04-16 18:06:22 -0700

    Emacs: Add orgmode package archive.

--- emacs/init.el
+++ emacs/init.el
@@ -23,6 +23,7 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 ;; Load and activate emacs packages. Do this first so that the
 ;; packages are loaded before you start trying to modify them.
