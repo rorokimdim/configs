b6c39bd295fe32c1899e841340a41081896430f6
Amit Shrestha <ashrestha@housecanary.com>
2017-09-21 23:34:22 -0700

    Clean up default lein profile.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,13 +1,6 @@
 {
   :user {
-    :plugins [
-      [lein-gorilla "0.4.0"]
-      [cider/cider-nrepl "0.15.0-SNAPSHOT"]
-    ]
-    :dependencies [
-      [lein-cljfmt "0.5.6"]
-      [org.clojure/tools.trace "0.7.9"]
-      [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-    ]
+    :plugins []
+    :dependencies []
   }
 }
