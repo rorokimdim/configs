09b0516066b9be4ba76008d63167b6efcca35f9b
Amit Shrestha <ashrestha@housecanary.com>
2017-11-30 20:29:57 -0800

    lein: Add lein-binplus plugin.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -5,6 +5,7 @@
                   [refactor-nrepl "2.4.0-SNAPSHOT"]
                   [thunknyc/nrepl-profile "0.1.0-SNAPSHOT"]
                   [lein-gorilla "0.4.0"]
+                  [lein-binplus "0.6.2"]
                   ]
         :dependencies [[jonase/eastwood "0.2.4" :exclusions [org.clojure/clojure]]
                        [cljfmt "0.5.1"]
