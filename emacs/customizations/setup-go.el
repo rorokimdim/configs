6afd9d2c78cb2ed462f6271429fc85e247307fd3
Amit Shrestha <ashrestha@housecanary.com>
2017-12-01 15:46:49 -0800

    Emacs: More go configuration.

--- emacs/customizations/setup-go.el
+++ emacs/customizations/setup-go.el
@@ -8,6 +8,7 @@
             (add-hook 'before-save-hook 'gofmt-before-save)
             (setq tab-width 4)
             (setq indent-tabs-mode 1)
+            (setq compile-command "go build")
             (add-to-list 'company-backends 'company-go)))
 
 ;; Shortcuts for go mode
@@ -18,6 +19,9 @@
   :evil-states (normal visual)
   :major-modes (go-mode)
   :bindings ("gd" 'go-guru-definition
-             ))
-
-
+             "i" 'go-import-add
+             "cc" (lambda ()
+                    (interactive)
+                    (call-interactively 'recompile)
+                    (select-window (get-buffer-window "*compilation*"))
+                    )))
