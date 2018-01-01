fe1399db7dcd42824828c43edadfecdf022ecdef
Amit Shrestha <ashrestha@housecanary.com>
2017-12-31 23:19:29 -0800

    lein: clean up lein profiles.clj.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,12 +4,10 @@
                   [cider/cider-nrepl "0.16.0-SNAPSHOT"]
                   [refactor-nrepl "2.4.0-SNAPSHOT"]
                   [thunknyc/nrepl-profile "0.1.0-SNAPSHOT"]
-                  [lein-gorilla "0.4.0"]
-                  [lein-binplus "0.6.2"]
-                  ]
+                  [lein-binplus "0.6.2"]]
+
         :dependencies [[jonase/eastwood "0.2.4" :exclusions [org.clojure/clojure]]
                        [cljfmt "0.5.1"]
-                       [compliment "0.3.4"]
-                       ]
+                       [compliment "0.3.4"]]
         }
  }
