175c782ee8016262b6f8bbfda80de61c5bd2b09b
Amit Shrestha <ashrestha@housecanary.com>
2019-10-27 13:45:14 -0700

    Emacs: Add slime stuff only when roswell is setup properly.

--- emacs/customizations/setup-clisp.el
+++ emacs/customizations/setup-clisp.el
@@ -1,26 +1,28 @@
-(setq inferior-lisp-program "ros -Q run")
-(setq slime-contribs '(slime-fancy))
-
 (when (file-exists-p "~/.roswell/helper.el")
-  (load (expand-file-name "~/.roswell/helper.el")))
+  (load (expand-file-name "~/.roswell/helper.el"))
 
-;; Shortcuts for common lisp mode
-(require 'bind-map)
-(bind-map my-clisp-mode-map
-  :keys ("s-,")
-  :evil-keys (",")
-  :evil-states (normal visual)
-  :major-modes (lisp-mode)
-  :bindings ("ee" (lambda ()
-                    (interactive)
-                    (forward-char)
-                    (slime-eval-last-expression))
-             "ef" 'slime-eval-defun
-             "eb" 'slime-eval-buffer
-             "er" 'slime-eval-region
-             "r"  'slime-repl))
+  (setq inferior-lisp-program "ros -Q run")
+  (setq slime-contribs '(slime-fancy))
 
-;; Shortcuts for slime repl
-(define-key slime-repl-mode-map (kbd "<up>") 'slime-repl-previous-input)
-(define-key slime-repl-mode-map (kbd "<down>") 'slime-repl-next-input)
-(define-key slime-repl-mode-map (kbd "C-l") 'slime-repl-clear-buffer)
+  ;; Shortcuts for slime repl
+  (define-key slime-repl-mode-map (kbd "<up>") 'slime-repl-previous-input)
+  (define-key slime-repl-mode-map (kbd "<down>") 'slime-repl-next-input)
+  (define-key slime-repl-mode-map (kbd "C-l") 'slime-repl-clear-buffer)
+
+  ;; Shortcuts for common lisp mode
+  (require 'bind-map)
+  (bind-map my-clisp-mode-map
+    :keys ("s-,")
+    :evil-keys (",")
+    :evil-states (normal visual)
+    :major-modes (lisp-mode)
+    :bindings ("ee" (lambda ()
+                      (interactive)
+                      (forward-char)
+                      (slime-eval-last-expression))
+               "ef" 'slime-eval-defun
+               "eb" 'slime-eval-buffer
+               "er" 'slime-eval-region
+               "r"  'slime-repl)))
+
+
