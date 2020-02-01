bdbc0c1db8283d7b8e8aa127965c5c91d8af6c00
Amit Shrestha <amit.p.shrestha@gmail.com>
2020-01-31 21:04:57 -0800

    Emacs: Some cleanup for haskell mode.

--- emacs/customizations/setup-haskell.el
+++ emacs/customizations/setup-haskell.el
@@ -3,12 +3,7 @@
 (setq haskell-stylish-on-save t)
 (setq haskell-mode-stylish-haskell-path "brittany")
 
-(setq flymake-no-changes-timeout nil)
-(setq flymake-start-syntax-check-on-newline nil)
-(setq flycheck-check-syntax-automatically '(save mode-enabled))
-
 (require 'lsp)
 (require 'lsp-haskell)
 
-(add-hook 'haskell-mode-hook 'flycheck-mode)
 (add-hook 'haskell-mode-hook #'lsp)
