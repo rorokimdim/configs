753f80d2429ab46fc910a82c7a26094c2c9c219b
Amit Shrestha <amit.p.shrestha@gmail.com>
2020-04-10 08:57:47 -0700

    Emacs: More configs for lsp and go.

--- emacs/customizations/setup-go.el
+++ emacs/customizations/setup-go.el
@@ -15,6 +15,11 @@
   (emamux:run-command (concat "clear && go run " (buffer-file-name))
                       (ffip-get-project-root-directory)))
 
+(defun my-go-run-tests ()
+  (interactive)
+  (emamux:run-command "clear && go test -v"
+                      (file-name-directory buffer-file-name)))
+
 (defun my-clear-terminal ()
   (interactive)
   (emamux:run-command "clear" (ffip-get-project-root-directory)))
@@ -25,13 +30,14 @@
   :evil-keys (",")
   :evil-states (normal visual)
   :major-modes (go-mode)
-  :bindings ("dd" 'lsp-ui-doc-glance
+  :bindings ("dd" 'my-lsp-ui-doc-toggle
              "gt" 'lsp-find-type-definition
              "gd" 'lsp-find-definition
              "gr" 'lsp-find-references
              "rr" 'my-go-run
              "re" 'emamux:close-panes
              "rl" 'my-clear-terminal
+             "rt" 'my-go-run-tests
              ))
 
 (add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
--- emacs/customizations/setup-lsp.el
+++ emacs/customizations/setup-lsp.el
@@ -11,10 +11,13 @@
 
   ;; flycheck doesn't seem to work (max depth exceeded error)
   (setq lsp-diagnostic-package :none)
+  (setq lsp-flycheck-live-reporting nil)
+
+  ;; symbol highlighting has too much visual distraction
+  (setq lsp-enable-symbol-highlighting nil)
 
   ;; sidelines are annoying most of the time
   (setq lsp-ui-sideline-enable nil)
-  (setq lsp-flycheck-live-reporting nil)
   (setq lsp-ui-sideline-enable nil)
   (setq lsp-ui-sideline-show-diagnostics nil))
 
@@ -25,3 +28,10 @@
   :commands company-lsp
   :config
   (push 'company-lsp company-backends))
+
+(defun my-lsp-ui-doc-toggle ()
+  "Toggles lsp-ui-doc."
+  (interactive)
+  (if (lsp-ui-doc--frame-visible-p)
+      (lsp-ui-doc-hide)
+    (lsp-ui-doc-glance)))
