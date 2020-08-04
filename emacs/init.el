6e116919496332b2fb9b040b73ee4dafe43aefda
Amit Shrestha <amit.p.shrestha@gmail.com>
2020-08-04 04:46:17 -0700

    Emacs: Change melpa host.

--- emacs/init.el
+++ emacs/init.el
@@ -58,7 +58,7 @@
 
 ;; Define package repositories
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (defvar my-packages
