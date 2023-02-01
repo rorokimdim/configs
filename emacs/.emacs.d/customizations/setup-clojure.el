4374d24485209b26366e48eff19be4be7a0e8300
Amit Shrestha <amit.p.shrestha@gmail.com>
2023-01-31 20:09:25 -0800

    emacs: clojure-mode disable clojure-use-backtracking-indent.

--- emacs/.emacs.d/customizations/setup-clojure.el
+++ emacs/.emacs.d/customizations/setup-clojure.el
@@ -21,7 +21,9 @@
 (defun my-clojure-mode-hook ()
   (clj-refactor-mode 1)
   (yas-minor-mode 1) ; for adding require/use/import statements
+  (setq clojure-use-backtracking-indent nil)
   (cljr-add-keybindings-with-prefix "C-c C-m"))
+
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
 ;; Shortcuts for clojure mode
