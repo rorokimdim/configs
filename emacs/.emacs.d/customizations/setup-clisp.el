6fe82095cec6ce8c64a139e5c869fd052bf205f1
Amit Shrestha <amit.p.shrestha@gmail.com>
2023-05-24 23:31:41 -0700

    Add back common lisp config.

---
+++ emacs/.emacs.d/customizations/setup-clisp.el
@@ -0,0 +1,39 @@
+(defun my/slime-repl ()
+  "Starts slime if not started and switch to slime repl."
+  (interactive)
+  (unless (get-process "SLIME Lisp")
+    (slime))
+  (slime-repl))
+
+(when (file-exists-p "~/.roswell/helper.el")
+  (load (expand-file-name "~/.roswell/helper.el"))
+
+  (setq inferior-lisp-program "ros -Q run")
+  (setq slime-contribs '(slime-fancy))
+
+  ;; Shortcuts for slime repl
+  (define-key slime-repl-mode-map (kbd "<up>") 'slime-repl-previous-input)
+  (define-key slime-repl-mode-map (kbd "<down>") 'slime-repl-next-input)
+  (define-key slime-repl-mode-map (kbd "C-l") 'slime-repl-clear-buffer)
+
+  ;; Shortcuts for lips-mode in editor
+  (define-key slime-mode-indirect-map (kbd "C-c SPC") 'slime-eval-last-expression)
+
+  (add-hook 'lisp-mode-hook 'highlight-defined-mode)
+  (add-hook 'lisp-mode-hook 'lisp-extra-font-lock-mode)
+
+  ;; Shortcuts for common lisp mode
+  (require 'bind-map)
+  (bind-map my/clisp-mode-map
+    :keys ("s-,")
+    :evil-keys (",")
+    :evil-states (normal visual)
+    :major-modes (lisp-mode)
+    :bindings ("ee" 'slime-eval-last-expression
+               "ef" 'slime-eval-defun
+               "eb" 'slime-eval-buffer
+               "er" 'slime-eval-region
+               "gd" 'slime-edit-definition
+               "hh" 'slime-documentation
+               "hb" 'slime-documentation-lookup
+               "r" 'my/slime-repl)))
